bcdc6df1e849ad9f5e5c528c202d5c7253c31e62
Andres Orozco <104736953+AndresOrozcoDev@users.noreply.github.com>
2025-10-24 17:02:31 -0400

    fix error

--- src/app/features/paginator/page/home/home.component.ts
+++ src/app/features/paginator/page/home/home.component.ts
@@ -31,6 +31,7 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.getStates();
     this.watchQueryParams();
+    this.getCities();
   }
 
   private watchQueryParams(): void {
